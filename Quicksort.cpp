8b9eb09e637a59294822bae1d1430df90893de72
Paula Ribeiro <paula.mribeiro05@gmail.com>
2019-05-26 19:38:07 -0300

    Adiciona algoritmo de ordenação decrescente

--- Quicksort.cpp
+++ Quicksort.cpp
@@ -1,4 +1,14 @@
 #include "Quicksort.h"
+#include <iostream>
+
+using namespace::std;
+
+int calculaMediana(int *i, int *j, int *lista);
+void ordenaCrescente(int *i, int *j, int *lista, int pivo);
+void ordenaDecrescente(int *i, int *j, int *lista, int pivo);
+int selecionaPivo(int tipoPivo, int *i, int *j, int *lista);
+void ordena(int esquerda, int direita, int *lista, int tipoPivo);
+void particao(int esquerda, int direita, int *i, int *j, int *lista, int tipoPivo);
 
 int calculaMediana(int *i, int *j, int *lista) {
     int inicio, meio, fim;
@@ -38,14 +48,8 @@ int selecionaPivo(int tipoPivo, int *i, int *j, int *lista) {
     }
 }
 
-void particao(int esquerda, int direita, int *i, int *j, int *lista, int tipoPivo) {
-    int pivo;
-    *i = esquerda;
-    *j = direita;
-
-    pivo = selecionaPivo(tipoPivo, i, j, lista);
-
-    do {
+void ordenaCrescente(int *i, int *j, int *lista, int pivo) {
+     do {
         while (pivo > lista[*i])
             (*i)++;
 
@@ -61,6 +65,35 @@ void particao(int esquerda, int direita, int *i, int *j, int *lista, int tipoPiv
         }
     } while (*i <= *j);
 }
+
+void ordenaDecrescente(int *i, int *j, int *lista, int pivo) {
+    do {
+        while (pivo < lista[*i])
+            (*i)++;
+
+        while (pivo > lista[*j])
+            (*j)--;
+
+        if (*i <= *j) {
+            int auxiliar = lista[*i];
+            lista[*i] = lista[*j];
+            lista[*j] = auxiliar;
+            (*i)++;
+            (*j)--;
+        }
+    } while (*i <= *j);
+}
+
+void particao(int esquerda, int direita, int *i, int *j, int *lista, int tipoPivo) {
+    int pivo;
+    *i = esquerda;
+    *j = direita;
+
+    pivo = selecionaPivo(tipoPivo, i, j, lista);
+    ordenaDecrescente(i, j, lista, pivo);
+}
+
+
 
 void ordena(int esquerda, int direita, int *lista, int tipoPivo) {
     int i, j;
--- Quicksort.h
+++ Quicksort.h
@@ -1,10 +1,6 @@
 #ifndef QUICKSORT_H
 #define QUICKSORT_H
 
-void particao(int esquerda, int direita, int *i, int *j, int *lista, int tipoPivo);
-void ordena(int esquerda, int direita, int *lista, int tipoPivo);
 void quicksort(int *lista, int tamanho, int tipoPivo);
-int selecionaPivo(int tipoPivo, int *i, int *j, int *lista);
-int calculaMediana(int *i, int *j, int *lista);
 
 #endif
